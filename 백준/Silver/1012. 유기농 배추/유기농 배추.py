c8a1e3df4429b4104185dbb3a234c3f898f858e1
Yongmin Park <701pooh@naver.com>
2022-09-18 11:00:00 +0000

    [Silver II] Title: 유기농 배추, Time: 104 ms, Memory: 32436 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,45 @@
+from collections import deque
+from sys import stdin
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+
+t = int(input()) # 테스트케이스 2개
+result_list = []
+
+def make_graph(m, n, k):
+    graph = [[0] * m for _ in range(n)]
+    for _ in range(k):
+        x, y = map(int, stdin.readline().split())
+        graph[y][x] = 1
+    return graph
+
+def bfs(y, x):
+
+    queue = deque([])
+    queue.append([y, x])
+
+    while queue:
+        y, x = queue.pop()
+        for d in range(4):
+            xx = dx[d] + x
+            yy = dy[d] + y
+            if 0 <= xx < m and 0 <= yy < n:
+                if graph[yy][xx] == 1:
+                    graph[yy][xx] = 0
+                    queue.append([yy, xx])
+
+
+for _ in range(t):
+    result = 0
+    m, n, k = map(int, stdin.readline().split()) # m:가로, n:세로
+    graph = make_graph(m, n, k)
+    for i in range(n): # 세로
+        for j in range(m): # 가로
+            if graph[i][j] == 1:
+                bfs(i, j)
+                result += 1
+    result_list.append(result)
+
+print(*result_list, sep='\n')
